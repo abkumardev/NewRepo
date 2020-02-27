996d9a6c06ec61816f9ac8c7e2e11d5a49b61b34
Abhishek Kumar <abkumar@innominds.com>
2020-02-27 13:59:29 +0530

    DI and generics

---
+++ TKT/TktSys.ZenPro/ZenDeskService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TktSys.Business.Entities;
+using TktSys.Utility.Common;
+
+namespace TktSys.ZenPro
+{
+    public class ZenDeskService : ITicketService
+    {
+        ZenDeskTicket zenDeskTicket;
+        public Guid CreateTicket(Request request)
+        {
+            Guid requestId = Guid.Empty;
+            zenDeskTicket = new ZenDeskTicket();
+            zenDeskTicket.EmployeeId = request.RequestResourceId;
+            zenDeskTicket.CreatedOn = request.CreatedDate;
+            GenericRepository<ZenDeskTicket> genericCRUD = new GenericRepository<ZenDeskTicket>();
+            return genericCRUD.Post(zenDeskTicket, "ZenDeskAPIUrl");
+        }
+    }
+}
